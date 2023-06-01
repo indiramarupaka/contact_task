09bbbf87352055a3c3e39b7609210ca12d872386
indira <indira.marpaka@gmail.com>
2023-06-01 21:19:22 +0530

    added changes

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import './App.css';
 import Contacts from './components/Contacts';
 import Header from './components/Header';
+import LargeOffer from './components/LargeOffer';
 
 function App() {
   return (
     <div className="App">
       <Header />
       <Contacts />
+      <LargeOffer />
     </div>
   );
 }
---
+++ src/components/LargeOffer.js
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function LargeOffer() {
+  return (
+    <div>LargeOffer</div>
+  )
+}
+
+export default LargeOffer
